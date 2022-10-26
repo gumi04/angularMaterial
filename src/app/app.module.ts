ce1008c8e9b82f5dca767094e6edb39a71356933
Gumaro Contreras Casillas <gumaro.contreras@proveedoresgnp.mx>
2022-10-26 13:27:18 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
+  { path: '**', redirectTo: 'contactmanager' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contactmanager/models/user.ts
@@ -0,0 +1,11 @@
+import { Note } from './notes';
+
+export class User {
+    id: number;
+    birthDate: Date;
+    name: string;
+    avatar: string;
+    bio: string;
+
+    notes: Note[] = [];
+}
